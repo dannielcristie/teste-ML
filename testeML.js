cc1d87a5b3a1a5c39015957e651ac7485b87a02e
Danniel Cristie <dany14012@gmail.com>
2022-08-02 15:20:53 -0300

    initial

---
+++ testeML.js
@@ -0,0 +1,31 @@
+function decomposeNumbers(maxDigit) {
+    let arrayValidValues = [];
+    let maxValue = maxDigit + maxDigit * 10 + maxDigit * 100 + maxDigit * 1000;
+    let minValue = 1000;
+
+    for (let i = minValue; i < maxValue; i++) {
+        let stringValue = i.toString();
+        let arr = stringValue.split("");
+
+        if ((arr[0] <= maxDigit) && (arr[1] <= maxDigit) && (arr[2] <= maxDigit) && arr[3] <= maxDigit) {
+
+            let sum = parseInt(arr[0]) + parseInt(arr[1]) + parseInt(arr[2]) + parseInt(arr[3]);
+
+            if (sum == 21) {
+                arrayValidValues.push(arr.join(""));
+            }
+
+        }
+    }
+
+
+    if (arrayValidValues.length > 0) {
+        for (i = 0; i < arrayValidValues.length; i++) {
+            console.log(arrayValidValues[i]);
+        }
+    } else {
+        console.log(null);
+    }
+}
+
+decomposeNumbers(8);
